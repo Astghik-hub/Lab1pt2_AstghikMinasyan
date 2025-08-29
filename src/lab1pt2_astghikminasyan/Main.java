0c847aabdd579bc403c07a3e8a16a1302ae4f3d1
2466920 <2466920@F40005-4812G24.VanierCollege.intra>
2025-08-29 10:44:56 -0400

    added getPointsEach() and getNumMissed() methods in FinalEaxam and wrote the code in main for the output

--- src/lab1pt2_astghikminasyan/Main.java
+++ src/lab1pt2_astghikminasyan/Main.java
@@ -14,7 +14,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int questions = 100;
+        int missed = 25;
+        FinalExam f1 = new FinalExam(questions, missed);
+        
+        System.out.printf("How many questions on the final exam? %d%n", questions);
+        System.out.printf("How many questions did the student miss? %d%n", f1.getNumMissed());
+        System.out.printf("Each question counts %.1f%n", f1.getPointsEach());
+        System.out.printf("The exam score is %.1f%n", f1.getScore());
+        System.out.printf("The exam grade is %c%n", f1.getGrade());
     }
-    
 }
