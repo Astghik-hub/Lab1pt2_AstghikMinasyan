b746bec6893819003eec4b2c54f39447e11f8edd
2466920 <2466920@F40005-4812G24.VanierCollege.intra>
2025-08-29 11:00:05 -0400

    created PassFailActivity, gave parameter, created constructor and overrid getGrade method in the superclass

---
+++ src/lab1pt2_astghikminasyan/PassFailActivity.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab1pt2_astghikminasyan;
+
+/**
+ *
+ * @author 2466920
+ */
+public class PassFailActivity extends GradedActivity {
+    private double minPassingScore;
+
+    /**
+     * Constructor
+     * @param minPassingScore the minimum passing grade for the activity 
+     */
+    public PassFailActivity(double minPassingScore) {
+        this.minPassingScore = minPassingScore;
+    }
+    
+    @Override
+    public char getGrade() {
+        char passFail;
+        
+        // Determine if passFail is 'P' or 'F'
+        if (super.getScore() > minPassingScore) {
+            passFail = 'P';
+        } else passFail = 'F';
+        
+        return passFail;
+    }
+}
